137a49a7ffd9f1685d5b73823eeb420ca8b029cc
valeriivoronkov <scorpirat@gmail.com>
2019-12-14 10:16:47 +0300

    Modify rentsite/settings.py TEMPLATES/DIRS add -- os.path.join(BASE_DIR, 'templates') -- in root add templates folder with html templates -- add jinja framework for rendering templates

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Test</h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
